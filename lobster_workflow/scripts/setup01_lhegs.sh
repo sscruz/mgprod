fe96712ee9440929707e555c9a06903fb4bae99b
Andrew42 <awightma@nd.edu>
2018-04-23 15:21:22 +0200

    Adjust setup scripts

--- lobster_workflow/scripts/setup01_lhegs.sh
+++ lobster_workflow/scripts/setup01_lhegs.sh
@@ -14,5 +14,5 @@ curl -s --insecure https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_frag
 
 scram b
 cd ../../
-cmsDriver.py Configuration/GenProduction/python/HIG-RunIIFall17wmLHEGS-00040-fragment.py --fileout file:HIG-RunIIFall17wmLHEGS-00040ND.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 93X_mc2017_realistic_v3 --beamspot Realistic25ns13TeVEarly2017Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename HIG-RunIIFall17wmLHEGS-00040_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 1412 || exit $? ; 
+#cmsDriver.py Configuration/GenProduction/python/HIG-RunIIFall17wmLHEGS-00040-fragment.py --fileout file:HIG-RunIIFall17wmLHEGS-00040ND.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 93X_mc2017_realistic_v3 --beamspot Realistic25ns13TeVEarly2017Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename HIG-RunIIFall17wmLHEGS-00040_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 1412 || exit $? ; 
 
--- lobster_workflow/scripts/setup03_maod_correctPU.sh
+++ lobster_workflow/scripts/setup03_maod_correctPU.sh
@@ -12,5 +12,5 @@ eval `scram runtime -sh`
 
 scram b
 cd ../../
-cmsDriver.py step1 --filein file:HIG-RunIIFall17DRPremix-00823ND.root --fileout file:HIG-RunIIFall17MiniAOD-00821ND.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 94X_mc2017_realistic_v11 --step PAT --nThreads 8 --era Run2_2017 --python_filename HIG-RunIIFall17MiniAOD-00821_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 30 || exit $? ; 
+#cmsDriver.py step1 --filein file:HIG-RunIIFall17DRPremix-00823ND.root --fileout file:HIG-RunIIFall17MiniAOD-00821ND.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 94X_mc2017_realistic_v11 --step PAT --nThreads 8 --era Run2_2017 --python_filename HIG-RunIIFall17MiniAOD-00821_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 30 || exit $? ; 
 
