4b3c2eda66e592566ad86992f53b36e5b1f4f143
Andrew42 <awightma@nd.edu>
2019-08-28 17:00:01 -0400

    Add missing WF settings and minor tweaks

--- lobster_workflow/lobster_centralProduction_config.py
+++ lobster_workflow/lobster_centralProduction_config.py
@@ -12,8 +12,8 @@ timestamp_tag = datetime.datetime.now().strftime('%Y%m%d_%H%M')
 events_per_gridpack = 50e3
 events_per_lumi = 500
 
-RUN_SETUP = 'mg_studies'
-#RUN_SETUP = 'lobster_test'
+RUN_SETUP = 'lobster_test'
+# RUN_SETUP = 'mg_studies'
 
 # Where the gridpacks are located
 input_path = "/store/user/"
@@ -29,11 +29,11 @@ if RUN_SETUP == 'mg_studies':
     workdir_path = "/tmpscratch/users/$USER/CentralProduction/{tag}/{ver}".format(tag=out_tag,ver=version)
     plotdir_path = "~/www/lobster/CentralProduction/{tag}/{ver}".format(tag=out_tag,ver=version)
 elif RUN_SETUP == 'lobster_test':
-    # For lobster workflow tests
-    out_tag = "lobster_{tstamp}".format(timestamp_tag)
-    output_path  = "/store/user/$USER/CentralProduction/tests/{tag}/{ver}".format(tag=out_tag,ver=version)
-    workdir_path = "/tmpscratch/users/$USER/CentralProduction/tests/{tag}/{ver}".format(tag=out_tag,ver=version)
-    plotdir_path = "~/www/lobster/CentralProduction/tests/{tag}/{ver}".format(tag=out_tag,ver=version)
+    # For lobster workflow testing
+    test_tag = "lobster_{tstamp}".format(tstamp=timestamp_tag)
+    output_path  = "/store/user/$USER/tests/{tag}".format(tag=test_tag)
+    workdir_path = "/tmpscratch/users/$USER/tests/{tag}".format(tag=test_tag)
+    plotdir_path = "~/www/lobster/tests/{tag}".format(tag=test_tag)
 else:
     print "Unknown run setup, %s" % (RUN_SETUP)
     raise ValueError
@@ -58,12 +58,10 @@ storage = StorageConfiguration(
     disable_input_streaming=True,
 )
 
-input_path_full = "/hadoop" + input_path
-
 tzq_info = {
     'name': 'tzq_ll_4f_ckm_NLO',
-    'gridpack_location': os.path.join(input_path_full,"awightma/gridpack_scans/central_gridpacks"),
-    'gridpack_name': 'tzq_ll_4f_ckm_NLO_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz',
+    'location': os.path.join(input_path,"awightma/gridpack_scans/central_gridpacks"),
+    'tarball': 'tzq_ll_4f_ckm_NLO_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz',
     'lhe_cfg': 'python_cfgs/central/tzq_ll_4f_ckm_NLO/LHE-00000_1_cfg.py',
     'gen_cfg': 'python_cfgs/central/tzq_ll_4f_ckm_NLO/GEN-00000_1_cfg.py',
     'lhe_release': 'CMSSW_9_3_6',
@@ -97,16 +95,20 @@ wf = []
 print "Generating workflows:"
 for idx,gp_info in enumerate(gridpacks):
     nevents = events_per_gridpack
-    gp_name = gp_info['gridpack_name']
     name = gp_info['name']
+    gp_loc = os.path.join(gp_info['location'],gp_info['tarball'])
 
-    print "\t[{0}/{1}] Gridpack: {gp} (nevts {events})".format(idx+1,len(gridpacks),gp=name,events=nevents)
+    print "[{0}/{1}] Gridpack: {gp} (nevts {events})".format(idx+1,len(gridpacks),gp=name,events=nevents)
+    print "\tGridpack: {path}".format(path=gp_loc)
 
     cmd = ['cmsRun']
     cmd.append(gp_info['lhe_cfg'])
+    label = 'lhe_step_{tag}'.format(tag=name)
 
+    print "\tLHE Step: {label}".format(label=label)
+    print "\tLHE cfg: {cfg}".format(cfg=gp_info['lhe_cfg'])
     lhe = Workflow(
-        label='lhe_step_{tag}'.format(tag=label_tag),
+        label=label,
         command=' '.join(cmd),
         sandbox=cmssw.Sandbox(release=gp_info['lhe_release']),
         merge_size=-1,
@@ -114,7 +116,7 @@ for idx,gp_info in enumerate(gridpacks):
         globaltag=False,
         outputs=['LHE-00000.root'],
         dataset=MultiProductionDataset(
-            gridpacks=,
+            gridpacks=gp_loc,
             events_per_gridpack=nevents,
             events_per_lumi=events_per_lumi,
             lumis_per_task=1,
@@ -125,9 +127,12 @@ for idx,gp_info in enumerate(gridpacks):
 
     cmd = ['cmsRun']
     cmd.append(gp_info['gen_cfg'])
+    label = 'gen_step_{tag}'.format(tag=name)
 
+    print "\tGEN Step: {label}".format(label=label)
+    print "\tGEN cfg: {cfg}".format(cfg=gp_info['gen_cfg'])
     gen = Workflow(
-        label='gen_step_{tag}'.format(tag=label_tag),
+        label=label,
         command=' '.join(cmd),
         sandbox=cmssw.Sandbox(release=gp_info['gen_release']),
         merge_size=-1,
