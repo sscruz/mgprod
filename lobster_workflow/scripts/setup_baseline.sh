a61484e64499c4f5ba3250584207fd51c571690e
Andrew42 <awightma@nd.edu>
2019-08-28 14:02:04 -0400

    Fix echo statements

--- lobster_workflow/scripts/setup_baseline.sh
+++ lobster_workflow/scripts/setup_baseline.sh
@@ -9,7 +9,7 @@
 export SCRAM_ARCH=slc7_amd64_gcc630
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 if [ -r CMSSW_9_3_1/src ] ; then
-    echo release CMSSW_9_3_1 already exists
+    echo "release CMSSW_9_3_1 already exists"
 else
     scram p CMSSW CMSSW_9_3_1
 fi
--- lobster_workflow/scripts/setup_cmssw.sh
+++ lobster_workflow/scripts/setup_cmssw.sh
@@ -4,7 +4,7 @@ export SCRAM_ARCH=slc7_amd64_gcc630
 echo "Setting up CMSSW_9_3_1"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 if [ -r CMSSW_9_3_1/src ] ; then 
-    echo release CMSSW_9_3_1 already exists
+    echo "release CMSSW_9_3_1 already exists"
 else
     scram p CMSSW CMSSW_9_3_1
 fi
@@ -19,7 +19,7 @@ cd ../../
 echo "Setting up CMSSW_9_4_0_patch1"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 if [ -r CMSSW_9_4_0_patch1/src ] ; then 
-    echo release CMSSW_9_4_0_patch1 already exists
+    echo "release CMSSW_9_4_0_patch1 already exists"
 else
     scram p CMSSW CMSSW_9_4_0_patch1
 fi
