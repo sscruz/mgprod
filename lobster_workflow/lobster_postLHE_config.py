b0d6e633be1da235dbe45e2be6e4216aed9bc861
Andrew42 <awightma@nd.edu>
2019-09-03 12:53:13 -0400

    Implement modification of GS cfg settings

--- lobster_workflow/lobster_postLHE_config.py
+++ lobster_workflow/lobster_postLHE_config.py
@@ -8,20 +8,31 @@ from lobster.core import AdvancedOptions, Category, Config, Dataset,ParentDatase
 sys.path.append(os.getcwd())
 from helpers.utils import regex_match
 
+MODIFIED_CFG_DIR = "python_cfgs/modified"
+
 timestamp_tag = datetime.datetime.now().strftime('%Y%m%d_%H%M')
 
-username = "awightma"
+input_path = "/store/user/"
+input_path_full = "/hadoop" + input_path
+
+# username = "awightma"
 
 #RUN_SETUP = 'local'
 #RUN_SETUP = 'full_production'
 RUN_SETUP = 'mg_studies'
 
-in_ver  = "v1"   # The version index for the INPUT directory
+# Note: The workflows in each of the input directories should all be uniquely named w.r.t each other
+input_dirs = [
+    os.path.join(input_path_full,"kmohrman/LHE_step/2019_04_19/ttHJet-xqcutStudies-xqcut10qCutTests/v1"),
+    os.path.join(input_path_full,"kmohrman/LHE_step/2019_04_19/HanModelNoctG16DttllScanpointsxqcutscan/v1")
+]
+
+# in_ver  = "v1"   # The version index for the INPUT directory
 out_ver = "v1"   # The version index for the OUTPUT directory
 
-grp_tag  = "2019_04_19/ttZRunCard"      # For 'local' and 'mg_studies' setups
-out_tag  = "2019_04_19/ttZRunCard"
-prod_tag = "Round1/Batch1"              # For 'full_production' setup
+# grp_tag  = "2019_04_19/ttZRunCard"      # For 'local' and 'mg_studies' setups
+out_tag = "2019_04_19/ttZRunCard"      # For 'mg_studies' setup
+out_tag = "Round1/Batch1"              # For 'full_production' setup
 
 # Only run over lhe steps from specific processes/coeffs/runs
 process_whitelist = []
@@ -33,27 +44,25 @@ master_label = 'EFT_ALL_postLHE_{tstamp}'.format(tstamp=timestamp_tag)
 if RUN_SETUP == 'local':
     # For quick generic lobster workflow testing
     test_tag = "lobster_{tstamp}".format(tstamp=timestamp_tag)
-    input_path   = "/store/user/{user}/LHE_step/{tag}/{ver}/".format(user=username,tag=grp_tag,ver=in_ver)
+    # input_path   = "/store/user/{user}/LHE_step/{tag}/{ver}/".format(user=username,tag=grp_tag,ver=in_ver)
     output_path  = "/store/user/$USER/tests/{tag}".format(tag=test_tag)
     workdir_path = "/tmpscratch/users/$USER/tests/{tag}".format(tag=test_tag)
     plotdir_path = "~/www/lobster/tests/{tag}".format(tag=test_tag)
 elif RUN_SETUP == 'mg_studies':
     # For MadGraph test studies
-    input_path   = "/store/user/{user}/LHE_step/{tag}/{ver}/".format(user=username,tag=grp_tag,ver=in_ver)
+    # input_path   = "/store/user/{user}/LHE_step/{tag}/{ver}/".format(user=username,tag=grp_tag,ver=in_ver)
     output_path  = "/store/user/$USER/postLHE_step/{tag}/{ver}".format(tag=out_tag,ver=out_ver)
     workdir_path = "/tmpscratch/users/$USER/postLHE_step/{tag}/{ver}".format(tag=out_tag,ver=out_ver)
     plotdir_path = "~/www/lobster/postLHE_step/{tag}/{ver}".format(tag=out_tag,ver=out_ver)
 elif RUN_SETUP == 'full_production':
     # For Large MC production
-    input_path   = "/store/user/{user}/FullProduction/{tag}/LHE_step/{ver}".format(user=username,tag=prod_tag,ver=in_ver)
-    output_path  = "/store/user/$USER/FullProduction/{tag}/postLHE_step/{ver}".format(tag=prod_tag,ver=out_ver)
-    workdir_path = "/tmpscratch/users/$USER/FullProduction/{tag}/postLHE_step/{ver}".format(tag=prod_tag,ver=out_ver)
-    plotdir_path = "~/www/lobster/FullProduction/{tag}/postLHE_step/{ver}".format(tag=prod_tag,ver=out_ver)
+    # input_path   = "/store/user/{user}/FullProduction/{tag}/LHE_step/{ver}".format(user=username,tag=prod_tag,ver=in_ver)
+    output_path  = "/store/user/$USER/FullProduction/{tag}/postLHE_step/{ver}".format(tag=out_tag,ver=out_ver)
+    workdir_path = "/tmpscratch/users/$USER/FullProduction/{tag}/postLHE_step/{ver}".format(tag=out_tag,ver=out_ver)
+    plotdir_path = "~/www/lobster/FullProduction/{tag}/postLHE_step/{ver}".format(tag=out_tag,ver=out_ver)
 else:
     print "Unknown run setup, {setup}".format(setup=RUN_SETUP)
     raise ValueError
-
-input_path_full = "/hadoop" + input_path
 
 storage = StorageConfiguration(
     input=[
@@ -74,18 +83,21 @@ storage = StorageConfiguration(
 )
 
 lhe_dirs = []
-for fd in os.listdir(input_path_full):
-    if fd.find('lhe_step_') < 0:
-        continue
-    arr = fd.split('_')
-    p,c,r = arr[2],arr[3],arr[4]
-    if len(regex_match([p],process_whitelist)) == 0:
-        continue
-    elif len(regex_match([c],coeff_whitelist)) == 0:
-        continue
-    elif len(regex_match([r],runs_whitelist)) == 0:
-        continue
-    lhe_dirs.append(fd)
+for path in input_dirs:
+    for fd in os.listdir(path):
+        if fd.find('lhe_step_') < 0:
+            continue
+        arr = fd.split('_')
+        p,c,r = arr[2],arr[3],arr[4]
+        if len(regex_match([p],process_whitelist)) == 0:
+            continue
+        elif len(regex_match([c],coeff_whitelist)) == 0:
+            continue
+        elif len(regex_match([r],runs_whitelist)) == 0:
+            continue
+        lhe_dirs.append(fd)
+        relpath = os.path.relpath(path,input_path_full)
+        lhe_dirs.append(os.path.join(relpath,fd))
 
 
 # Old setup
@@ -127,7 +139,7 @@ for fd in os.listdir(input_path_full):
 #   Memory: 16000 | 8000
 #   Disk:   13000 | 6500
 #################################################################
-# Need to be careful with using 'runetime' setting, as it can cause us to exceed the workers resources
+# Need to be careful with using 'runtime' setting, as it can cause us to exceed the workers resources
 gs_resources = Category(
     name='gs',
     cores=6,
@@ -203,85 +215,106 @@ fragment_map = {
     },
     'tHq4fMatched': {# Uses same fragment as tllq4f
         'gs':   'python_cfgs/GS/HIG-RunIIFall17wmGS-00000-tllq4f_1_cfg.py',
-
     }
-
 }
+
+# Note: These changes will be applied to every process in the run
+# TODO: Allow ability to specify mods on per processes basis
+gs_mods = {}
+gs_mods['base'] = []
+gs_mods['qCut25'] = ['s|JetMatching:qCut = 19|JetMatching:qCut = 25|g']
 
 wf = []
 
 print "Generating workflows:"
 for idx,lhe_dir in enumerate(lhe_dirs):
     print "\t[{0}/{1}] LHE Input: {dir}".format(idx+1,len(lhe_dirs),dir=lhe_dir)
-    arr = lhe_dir.split('_')
+    # arr = lhe_dir.split('_')
+    head,tail = os.path.split(lhe_dir)
+    arr = tail.split('_')
     p,c,r = arr[2],arr[3],arr[4]
+    for mod_tag,sed_str_list in gs_mods.iteritems():
+        wf_fragments = {}
+        for step in wf_steps:
+            if fragment_map.has_key(p) and fragment_map[p].has_key(step):
+                template_loc = fragment_map[p][step]
+            else:
+                template_loc = fragment_map['default'][step]
+            # Only the GEN-SIM step can be modified
+            if step == 'gs':
+                head,tail = os.path.split(template_loc)
+                # This should be a unique identifier within a single lobster master to ensure we dont overwrite a cfg file too early
+                cfg_tag = '{tag}-{idx}'.format(tag=mod_tag,idx=idx)
+                tail = tail.replace("cfg.py","{tag}_cfg.py".format(tag=cfg_tag))
+                mod_loc = os.path.join(MODIFIED_CFG_DIR,tail)
+                shutil.copy(template_loc,mod_loc)
+                for sed_str in sed_str_list:
+                    if sed_str:
+                        run_process(['sed','-i','-e',sed_str,mod_loc])
+            else:
+                mod_loc = template_loc
+            wf_fragments[step] = mod_loc
+        if mod_tag == 'base': mod_tag = ''
+        label_tag = "{p}_{c}{mod}_{r}".format(p=p,c=c,r=r,mod=mod_tag)
+        gs = Workflow(
+            label='gs_step_{tag}'.format(tag=label_tag),
+            command='cmsRun {cfg}'.format(cfg=wf_fragments['gs']),
+            sandbox=cmssw.Sandbox(release='CMSSW_9_3_1'),
+            merge_size=-1,  # Don't merge files we don't plan to keep
+            cleanup_input=False,
+            globaltag=False,
+            outputs=['HIG-RunIIFall17wmLHEGS-00040ND.root'],
+            dataset=Dataset(
+                files=lhe_dir,
+                files_per_task=1,
+                patterns=["*.root"]
+            ),
+            category=gs_resources
+        )
 
-    wf_fragments = {}
-    for step in wf_steps:
-        if fragment_map.has_key(p) and fragment_map[p].has_key(step):
-            wf_fragments[step] = fragment_map[p][step]
-        else:
-            wf_fragments[step] = fragment_map['default'][step]
+        digi = Workflow(
+            label='digi_step_{tag}'.format(tag=label_tag),
+            command='cmsRun {cfg}'.format(cfg=wf_fragments['digi']),
+            sandbox=cmssw.Sandbox(release='CMSSW_9_4_0_patch1'),
+            merge_size=-1,  # Don't merge files we don't plan to keep
+            cleanup_input=False,    # Save the GEN-SIM step
+            outputs=['HIG-RunIIFall17DRPremix-00823ND_step1.root'],
+            dataset=ParentDataset(
+                parent=gs,
+                units_per_task=1
+            ),
+            category=digi_resources
+        )
 
-    gs = Workflow(
-        label='gs_step_{p}_{c}_{r}'.format(p=p,c=c,r=r),
-        command='cmsRun {cfg}'.format(cfg=wf_fragments['gs']),
-        sandbox=cmssw.Sandbox(release='CMSSW_9_3_1'),
-        merge_size=-1,  # Don't merge files we don't plan to keep
-        cleanup_input=False,
-        globaltag=False,
-        outputs=['HIG-RunIIFall17wmLHEGS-00040ND.root'],
-        dataset=Dataset(
-            files=lhe_dir,
-            files_per_task=1,
-            patterns=["*.root"]
-        ),
-        category=gs_resources
-    )
+        reco = Workflow(
+            label='reco_step_{tag}'.format(tag=label_tag),
+            command='cmsRun {cfg}'.format(cfg=wf_fragments['reco']),
+            sandbox=cmssw.Sandbox(release='CMSSW_9_4_0_patch1'),
+            merge_size=-1,  # Don't merge files we don't plan to keep
+            cleanup_input=True,
+            outputs=['HIG-RunIIFall17DRPremix-00823ND.root'],
+            dataset=ParentDataset(
+                parent=digi,
+                units_per_task=2
+            ),
+            category=reco_resources
+        )
 
-    digi = Workflow(
-        label='digi_step_{p}_{c}_{r}'.format(p=p,c=c,r=r),
-        command='cmsRun {cfg}'.format(cfg=wf_fragments['digi']),
-        sandbox=cmssw.Sandbox(release='CMSSW_9_4_0_patch1'),
-        merge_size=-1,  # Don't merge files we don't plan to keep
-        cleanup_input=False,    # Save the GEN-SIM step
-        outputs=['HIG-RunIIFall17DRPremix-00823ND_step1.root'],
-        dataset=ParentDataset(
-            parent=gs,
-            units_per_task=1
-        ),
-        category=digi_resources
-    )
+        maod = Workflow(
+            label='mAOD_step_{tag}'.format(tag=label_tag),
+            command='cmsRun {cfg}'.format(cfg=wf_fragments['maod']),
+            sandbox=cmssw.Sandbox(release='CMSSW_9_4_0_patch1'),
+            merge_size='256M',
+            cleanup_input=True,
+            outputs=['HIG-RunIIFall17MiniAOD-00821ND.root'],
+            dataset=ParentDataset(
+                parent=reco,
+                units_per_task=3
+            ),
+            category=maod_resources
+        )
 
-    reco = Workflow(
-        label='reco_step_{p}_{c}_{r}'.format(p=p,c=c,r=r),
-        command='cmsRun {cfg}'.format(cfg=wf_fragments['reco']),
-        sandbox=cmssw.Sandbox(release='CMSSW_9_4_0_patch1'),
-        merge_size=-1,  # Don't merge files we don't plan to keep
-        cleanup_input=True,
-        outputs=['HIG-RunIIFall17DRPremix-00823ND.root'],
-        dataset=ParentDataset(
-            parent=digi,
-            units_per_task=2
-        ),
-        category=reco_resources
-    )
-
-    maod = Workflow(
-        label='mAOD_step_{p}_{c}_{r}'.format(p=p,c=c,r=r),
-        command='cmsRun {cfg}'.format(cfg=wf_fragments['maod']),
-        sandbox=cmssw.Sandbox(release='CMSSW_9_4_0_patch1'),
-        merge_size='512M',
-        cleanup_input=True,
-        outputs=['HIG-RunIIFall17MiniAOD-00821ND.root'],
-        dataset=ParentDataset(
-            parent=reco,
-            units_per_task=3
-        ),
-        category=maod_resources
-    )
-
-    wf.extend([gs,digi,reco,maod])
+        wf.extend([gs,digi,reco,maod])
 
 config = Config(
     label=master_label,
