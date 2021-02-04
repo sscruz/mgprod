310fe97eba71b8667b6508768f20296520a7c1e4
Kelci Mohrman <kmohrman@earth.crc.nd.edu>
2021-02-04 12:26:11 -0500

    Adding more releases to setup_cmssw script

--- lobster_workflow/scripts/setup_cmssw.sh
+++ lobster_workflow/scripts/setup_cmssw.sh
@@ -24,8 +24,14 @@ setup_rel(){
 ( setup_rel CMSSW_9_4_0_patch1 )
 ( setup_rel CMSSW_9_3_6 )
 
-# For the UL16 samples
+# For the UL16 cfgs
 ( setup_rel CMSSW_10_6_17_patch1 )
 ( setup_rel CMSSW_8_0_33_UL )
 ( setup_rel CMSSW_10_6_20 )
 ( setup_rel CMSSW_10_6_19_patch2 )
+
+# For the UL17 cfgs (releases that are not already in UL16)
+( setup_rel CMSSW_9_4_14_UL_patch1 )
+
+# For the UL18 cfgs (releases that are not already in UL16)
+( setup_rel CMSSW_10_2_16_UL )
